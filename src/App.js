75e167039ee5c22e9a0fe861bd78dd72d94b10db
Erhan <teacherhan@gmail.com>
2020-11-21 02:03:40 -0800

    routes and components are added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="App">
-      GOOD MERNING!!!
-    </div>
+    <Router>
+       <Navbar />
+       <br/>
+       <Route path="/" exact component={ExercisesList}/>
+       <Route path="/edit/:id"  component={EditExercise}/>
+       <Route path="/create"  component={CreateExercise}/>
+       <Route path="/user"  component={CreateUser}/>
+    </Router>
+   
   );
 }
 
